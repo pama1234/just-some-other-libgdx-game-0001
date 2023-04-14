a501db0b774d90b1d066dc26a196b285f05084bb
pama1234 <62501730+pama1234@users.noreply.github.com>
2023-04-14 17:56:01 +0800

    init

---
+++ server/src/main/java/pama1234/gdx/game/app/server/ServerLauncher.java
@@ -0,0 +1,8 @@
+package pama1234.gdx.game.app.server;
+
+/** Launches the server application. */
+public class ServerLauncher {
+	public static void main(String[] args) {
+		// TODO Implement server application.
+	}
+}
