5a8284959f0a70f5f154cd92abd4aeaf6f1401ef
Pama1234 <pama1234@163.com>
2023-04-17 11:11:08 +0800

    update

--- server/src/main/java/pama1234/gdx/game/app/server/ServerLauncher.java
+++
@@ -1,8 +0,0 @@
-package pama1234.gdx.game.app.server;
-
-/** Launches the server application. */
-public class ServerLauncher {
-	public static void main(String[] args) {
-		// TODO Implement server application.
-	}
-}
